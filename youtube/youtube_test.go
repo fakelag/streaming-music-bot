f87d8bdaef85d413c6f5521709aed602ef852fea
fakelag <35497506+fakelag@users.noreply.github.com>
2024-01-15 21:43:06 +0200

    Add tests for ytdlp errors

--- youtube/youtube_test.go
+++ youtube/youtube_test.go
@@ -1,6 +1,8 @@
 package youtube
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 
 type MockCommandExecutor struct {
 	MockStdoutResult string
+	MockExitCode     int
 }
 
 func (command *MockCommandExecutor) RunCommandWithTimeout(
@@ -21,7 +24,11 @@ func (command *MockCommandExecutor) RunCommandWithTimeout(
 	errorChannel := make(chan error, 1)
 
 	go func() {
-		resultChannel <- &command.MockStdoutResult
+		if command.MockExitCode != 0 {
+			errorChannel <- errors.New(fmt.Sprintf("exit status %d", command.MockExitCode))
+		} else {
+			resultChannel <- &command.MockStdoutResult
+		}
 	}()
 
 	return resultChannel, errorChannel
@@ -51,5 +58,32 @@ var _ = Describe("Downloading from YT", func() {
 		Expect(media.ID).To(Equal("123"))
 		Expect(media.Title).To(Equal("Mock Title"))
 		Expect(media.StreamURL).To(Equal("url123"))
+	})
+
+	It("Fails with a sensible error if the download fails with an exit code", func() {
+		mockExecutor := &MockCommandExecutor{
+			MockStdoutResult: "",
+			MockExitCode:     1,
+		}
+
+		yt := &Youtube{
+			executor: mockExecutor,
+		}
+
+		_, err := yt.GetYoutubeMedia("foo")
+		Expect(err).To(MatchError("exit status 1"))
+	})
+
+	It("Fails with a sensible error if the download fails due to invalid json", func() {
+		mockExecutor := &MockCommandExecutor{
+			MockStdoutResult: "url123\n{",
+		}
+
+		yt := &Youtube{
+			executor: mockExecutor,
+		}
+
+		_, err := yt.GetYoutubeMedia("foo")
+		Expect(err).To(MatchError("unexpected end of JSON input"))
 	})
 })
